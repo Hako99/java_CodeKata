cfab6a9bd04de74582ba12a286057e4111b44637
mine <mineki94@naver.com>
2024-01-05 17:51:53 +0900

    3번문제

---
+++ src/a240105/a03/Main.java
@@ -0,0 +1,22 @@
+package a240105.a03;
+//문제 설명
+//        정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//0 < num1 ≤ 100
+//0 < num2 ≤ 100
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120805
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = (int) (Math.random()*100);
+        int num2 = (int) (Math.random()*100);
+
+        int answer = 0;
+        answer = num1 / num2;
+        System.out.println("num1 : "+ num1 + " , num2 : "+num2);
+        System.out.println(answer);
+
+    }
+}
