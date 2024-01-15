9b4ee337961c2f26279a9d25af51e6b4bc7b07aa
mine <mineki94@naver.com>
2024-01-15 20:30:55 +0900

    27번 문제

---
+++ src/a240115/a27/Main.java
@@ -0,0 +1,40 @@
+package a240115.a27;
+/*
+문제 설명
+프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
+전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+
+제한 조건
+phone_number는 길이 4 이상, 20이하인 문자열입니다.
+
+https://school.programmers.co.kr/learn/courses/30/lessons/12948
+
+ */
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = (int) (Math.random() * 16)+4;
+        char[] p = new char[a];
+        for (int i = 0 ; i < p.length ; i ++){
+            p[i] = (char) ((Math.random() * 9)+'0');
+        }
+        String phone_number = new String(p);
+
+        char[] phone = phone_number.toCharArray();  // String 을 char 배열로 변환
+
+        for ( int i = 0; i<phone.length-4;i++){     // 폰번호길이의 4개 적게까지만 루프
+            phone[i] = '*';                         // 맨 앞번호 부터 *로 바꾸기 반복
+        }
+        String answer = new String(phone);          // 다시 char 배열을 String으로 되돌림
+
+        System.out.println(Arrays.toString(p));
+        System.out.println(phone_number);
+        System.out.println(answer);
+
+
+
+
+    }
+}
